ac9a5958138475893e31204944db950c338d03d1
skscharr <skscharr@gmail.com>
2013-04-09 20:40:48 -0400

    figured out issue with rendering multiple dictionaries per view, began integration with linkedIn and twitter apis

--- web_api_calls/views.py
+++ web_api_calls/views.py
@@ -10,23 +10,26 @@ def tumblr_api(request, template='web_api_calls.html'):
 
     blog_basic_info = requests.get('http://api.tumblr.com/v2/blog/skscharr.tumblr.com/info', params=api_key)
     bbi = blog_basic_info.json()
-    #return render(request, template, {'blog_basic_info':bbi})
 
     blog_posts = requests.get('http://api.tumblr.com/v2/blog/skscharr.tumblr.com/posts', params=api_key)
     bp = blog_posts.json()
-    return render(request, template, {'blog_posts':bp})
 
-#def linkedin_api(request, template='web_api_calls.html'):
+    return render(request, template, {'blog_posts':bp, 'blog_basic_info':bbi})
 
+'''def linkedin_api(request, template='web_api_calls.html'):
+    env_api = os.environ.get('LINKEDIN_API_KEY', None)
+    env_sec = os.environ.get('LINKEDIN_SECRET_KEY', None)
 
-#def twitter_api(request, template='web_api_calls.html'):
-
+def twitter_api(request, template='web_api_calls.html'):
+    my_tweets = requests.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=16sks')
+    mt = my_tweets.json()
+    return render(request, template, {'my_tweets':mt})'''
 
 def github_api(request, template='web_api_calls.html'):
   events_performed = requests.get('https://api.github.com/users/skscharr/events')
   ep = events_performed.json()
-  return render(request, template, {'events_performed':ep})
 
   events_received = requests.get('https://api.github.com/users/skscharr/received_events')
   er = events_received.json()
-  return render(request, template, {'events_received':er})
+  
+  return render(request, template, {'events_received':er, 'events_performed':ep})
